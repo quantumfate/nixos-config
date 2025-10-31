f333ecb2534175011c25de1c1d3872ceb77fbbfa
Leon Connor Holm <leonconnorholm@gmail.com>
2025-10-31 22:28:27 +0100

    Added bash

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -1,4 +1,4 @@
 {
 
-  imports = [ ./hyprland ./git.nix ./ssh.nix ./bash.nix ];
+  imports = [ ./hyprland ./git.nix ./ssh.nix ];
 }
--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -2,5 +2,9 @@
   imports = [
     ./main-user.nix
     ./nvidia.nix
+    ./bash.nix
+    ./hyprland.nix
+    ./audio.nix
+    ./kernel.nix
   ]
 }
