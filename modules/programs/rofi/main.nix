3b57157b4f434ab030e1c7a67cd5bc629c5c4b4b
Leon Connor Holm <leonconnorholm@gmail.com>
2025-12-01 18:03:18 +0100

    Rofi

--- modules/programs/rofi/main.nix
+++ modules/programs/rofi/main.nix
@@ -7,10 +7,23 @@ in {
     programs.rofi = {
       enable = true;
       terminal = "/etc/profiles/per-user/${userCfg.name}/bin/foot";
-      font = "${config.common.style.fontFamily}";
+      font = "${config.common.style.fontFamily} 16";
       cycle = true;
       location = "center";
       modes = [ "run" "drun" "window" ];
+      extraConfig = {
+        icon-theme = "Papirus-dark";
+        drun-display-format = "{icon} {name}";
+        lines = 5;
+        hide-scrollbar = true;
+        display-drun = " Apps ";
+        display-run = " Run ";
+        display-window = " Window ";
+        sidebar-mode = true;
+        sorting-method = "fzf";
+        disable-history = true;
+        show-icons = true;
+      };
     };
   };
 }
--- modules/programs/rofi/theme.nix
+++ modules/programs/rofi/theme.nix
@@ -1,26 +1,101 @@
-{ config, lib, ... }:
+{ config, ... }:
 
 let userCfg = config.common.user;
 in {
-  home-manager.users."${userCfg.name}" = {
+  home-manager.users."${userCfg.name}" = { config, ... }: {
     # https://mynixos.com/options/programs.rofi
     programs.rofi = {
-      theme = let inherit (lib.formats.rasi) mkLiteral;
+      theme = let inherit (config.lib.formats.rasi) mkLiteral;
       in {
-        "*" = {
-          background-color = mkLiteral "#000000";
-          foreground-color = mkLiteral "rgba ( 250, 251, 252, 100 % )";
-          border-color = mkLiteral "#FFFFFF";
-          width = 512;
+        "*" = { };
+        "element-text, element-icon , mode-switcher" = {
+          background-color = mkLiteral "inherit";
+          text-color = mkLiteral "inherit";
         };
-        #"@theme" = "custom";
-        "#inputbar" = { children = map mkLiteral [ "prompt" "entry" ]; };
-
+        "window" = {
+          height = 600;
+          width = 500;
+          border = 2;
+          border-radius = 8;
+          border-color = mkLiteral "@mauve";
+          background-color = mkLiteral "@base";
+          padding = 10;
+        };
+        #"mainbox" = { background-color = mkLiteral "@base"; };
+        "#inputbar" = {
+          children = map mkLiteral [ "prompt" "entry" ];
+          #background-color = mkLiteral "@base";
+          border-radius = 8;
+          padding = 0;
+        };
+        "prompt" = {
+          #background-color = mkLiteral "@green";
+          padding = 4;
+          #text-color = mkLiteral "@text";
+          border-radius = 8;
+          margin = mkLiteral "10px 0px 10px 10px";
+        };
         "#textbox-prompt-colon" = {
           expand = false;
           str = ":";
           margin = mkLiteral "0px 0.3em 0em 0em";
-          text-color = mkLiteral "@foreground-color";
+          #text-color = mkLiteral "@foreground-color";
+        };
+        "entry" = {
+          padding = 6;
+          margin = mkLiteral "10px 10px 10px 10px";
+          #text-color = mkLiteral "@text";
+          #background-color = mkLiteral "@base";
+          border-radius = 8;
+        };
+        "listview" = {
+          border = 0;
+          padding = mkLiteral "6px 0 px 0px";
+          margin = mkLiteral "10px 0px 0px 6px";
+          columns = 1;
+          #background-color = mkLiteral "@base";
+          cycle = true;
+          scrollbar = false;
+        };
+        "element" = {
+          padding = mkLiteral "8px";
+          margin = mkLiteral "0px 10px 4px 4px";
+          #background-color = mkLiteral "@base"; # tODO change
+          #text-color = mkLiteral "@surface0";
+        };
+
+        "element-icon" = { size = 28; };
+
+        "element" = { border-radius = 8; };
+
+        "element selected.normal" = {
+          background-color = mkLiteral "@mauve";
+          text-color = mkLiteral "@crust";
+        };
+
+        "element selected.urgent" = {
+          background-color = mkLiteral "@red";
+          text-color = mkLiteral "@crust";
+        };
+
+        "element selected.active" = {
+          background-color = mkLiteral "@mauve";
+          text-color = mkLiteral "@crust";
+        };
+        "mode-switcher" = { spacing = 0; };
+
+        "button" = {
+          padding = 10;
+          #background-color = mkLiteral "@crust";
+          border-radius = 8;
+          #text-color = mkLiteral "@mantle";
+          vertical-align = mkLiteral "0.5";
+          horizontal-align = mkLiteral "0.5";
+        };
+
+        "button selected" = {
+          background-color = mkLiteral "@mauve";
+          text-color = mkLiteral "@crust";
         };
       };
     };
