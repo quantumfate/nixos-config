e4927755d5ee498769f15def9d4677cee9c2f288
Leon Connor Holm <leonconnorholm@gmail.com>
2025-12-03 14:13:26 +0100

    Hyprshot wrapper script

--- modules/programs/hyprland/scripts/hyprshot.sh
+++ modules/programs/hyprland/scripts/hyprshot.sh
@@ -3,8 +3,7 @@
 if [[ "$1" == "--region" ]]; then
     hyprshot -m region
 elif [[ "$1" == "--output" ]]; then
-    hyprshot --mode output --mode $(hyprctl -j monitors | jq -r '.[] | select(.focu
-sed) | "\(.name)"')
+    hyprshot --mode output --mode $(hyprctl -j monitors | jq -r '.[] | select(.focused) | "\(.name)"')
 elif [[ "$1" == "--window" ]]; then
     hyprshot -m window
 else
