c8daec62356738a346ee8591303863813ad7da94
Leon Connor Holm <leonconnorholm@gmail.com>
2025-12-03 21:23:19 +0100

    Dofus navigation script and binding

--- modules/programs/hyprland/scripts/dofus_toggle_launch.sh
+++ modules/programs/hyprland/scripts/dofus_toggle_launch.sh
@@ -1,20 +1,15 @@
 #!/bin/sh
 
 APP_NAME="Dofus Launch"
-SCRIPT_NAME="dofuslaunch.sh" # Ensure this matches your persistent script's name
+SCRIPT_NAME="dofuslaunch.sh"
 
-# 1. Try to find the running process
 PID=$(pgrep -f "$SCRIPT_NAME")
 
 if [ -n "$PID" ]; then
-    # Script is running, so kill it (toggle OFF)
     kill $PID
-    # Optional: Wait briefly to ensure termination before sending notification
     sleep 0.1
     notify-send --app-name="$APP_NAME" -h string:x-canonical-private-synchronous:sys-notify -u low "Script STOPPED"
 else
-    # Script is not running, so start it (toggle ON)
-    # Note: Use '&' to run it in the background so the wrapper script exits immediately
     ${XDG_CONFIG_HOME}/hypr/scripts/${SCRIPT_NAME} &
     notify-send --app-name="$APP_NAME" -h string:x-canonical-private-synchronous:sys-notify -u low "Script STARTED"
 fi
