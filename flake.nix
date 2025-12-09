fcebbb9a1c8a3e8329a603d6be3165f3725f66db
Leon Connor Holm <leonconnorholm@gmail.com>
2025-12-09 12:02:40 +0100

    Nix Flake update

--- flake.nix
+++ flake.nix
@@ -41,13 +41,13 @@
         ];
       };
     in {
-      nix.settings = {
-        substituters = [ "https://hyprland.cachix.org" ];
-        trusted-substituters = [ "https://hyprland.cachix.org" ];
-        trusted-public-keys = [
-          "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-        ];
-      };
+      #nix.settings = {
+      #  substituters = [ "https://hyprland.cachix.org" ];
+      #  trusted-substituters = [ "https://hyprland.cachix.org" ];
+      #  trusted-public-keys = [
+      #    "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      #  ];
+      #};
       nixosConfigurations.quantum-desktop = nixpkgs.lib.nixosSystem
         (mkSystem { configNix = ./hosts/quantum-desktop/configuration.nix; });
       nixosConfigurations.quantum-laptop = nixpkgs.lib.nixosSystem
