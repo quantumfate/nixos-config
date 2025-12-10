506f6f5d35272e2fca198354c4232c7c6d607d6d
Leon Connor Holm <leonconnorholm@gmail.com>
2025-12-10 11:08:35 +0100

    Nix Flake Update

--- install.sh
+++ install.sh
@@ -10,14 +10,14 @@ if [[ "$1" == "--upgrade" || "$1" == "-u" ]]; then
     sudo nixos-rebuild switch --upgrade $ADDITIONAL_FLAGS --flake "$HOME/nixos-config#$HOST"
 elif [[ "$1" == "--rebuild" || "$1" == "-r" ]]; then
     pkill zen-twilight
-    sudo nixos-rebuild switch --flake $ADDITIONAL_FLAGS "$HOME/nixos-config#$HOST"
+    sudo nixos-rebuild switch $ADDITIONAL_FLAGS --flake "$HOME/nixos-config#$HOST"
 elif [[ "$1" == "--flake-update" || "$1" == "-f" ]]; then
     nix flake update
     git add $HOME/nixos-config
     git commit -m "Nix Flake Update"
     git push
     pkill zen-twilight
-    sudo nixos-rebuild switch --flake --upgrade $ADDITIONAL_FLAGS "$HOME/nixos-config#$HOST"
+    sudo nixos-rebuild switch --upgrade $ADDITIONAL_FLAGS --flake "$HOME/nixos-config#$HOST"
 else
 	echo "usage: ./install.sh [-u | --upgrade] [-r | --rebuild] [-f | --flake-update ] [<args passed to nixos-rebuild switch>]"
 fi
