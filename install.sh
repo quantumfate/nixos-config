772762ba397709b85bf2203efa966aedfbb6ee4d
Leon Connor Holm <leonconnorholm@gmail.com>
2025-12-10 10:59:45 +0100

    Nix Flake Update

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 ADDITIONAL_FLAGS=${2:""}
+cd "$HOME/nixos-config" || exit 1
 
 if [[ "$1" == "--upgrade" || "$1" == "-u" ]]; then
     pkill zen-twilight
@@ -8,6 +9,13 @@ if [[ "$1" == "--upgrade" || "$1" == "-u" ]]; then
 elif [[ "$1" == "--rebuild" || "$1" == "-r" ]]; then
     pkill zen-twilight
     sudo nixos-rebuild switch --flake $ADDITIONAL_FLAGS "$HOME/nixos-config#quantum-desktop"
+elif [[ "$1" == "--flake-update" || "$1" == "-f" ]]; then
+    nix flake update
+    git add $HOME/nixos-config
+    git commit -m "Nix Flake Update"
+    git push
+    pkill zen-twilight
+    sudo nixos-rebuild switch --flake $ADDITIONAL_FLAGS "$HOME/nixos-config#quantum-desktop"
 else
-	echo "usage: ./install.sh [-u | --upgrade] [-r | --rebuild] [<args passed to nixos-rebuild switch>]"
+	echo "usage: ./install.sh [-u | --upgrade] [-r | --rebuild] [-f | --flake-update ] [<args passed to nixos-rebuild switch>]"
 fi
