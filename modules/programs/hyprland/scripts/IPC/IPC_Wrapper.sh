2d739c7bf56f9e37eb20cfc0e98793185268df2c
Leon Connor Holm <leonconnorholm@gmail.com>
2025-12-12 16:40:02 +0100

    Implemented an opacity switch for Zen Browser

--- modules/programs/hyprland/scripts/IPC/IPC_Wrapper.sh
+++ modules/programs/hyprland/scripts/IPC/IPC_Wrapper.sh
@@ -4,26 +4,23 @@ APP_NAME="IPC Wrapper"
 SCRIPTDIR=${XDG_CONFIG_HOME}/hypr/scripts
 IPC_SCRIPTDIR=${SCRIPTDIR}/IPC
 
-# if windowtitlev2
-# get adress
-# check if window is of class zen-browser
-# is title crunchyroll?
-# yes? override opacity to 1.0
-# no? override opacity to 0.1
-
 identify_window_initial_class() {
-    window_address="0x$(cut -d ',' -f1 <<< $(cut -d '>' -f2 <<< $1))"
-    window_title="$(cut -d ',' -f2 <<< $(cut -d '>' -f2 <<< $1))"
-    class=hyprctl -j clients | jq -r " .[] | select(.address == \"$window_address\") | \(.initialClass)"
+    window_address="0x$(cut -d ',' -f1 <<< $(cut -d '>' -f3 <<< $1))"
+    window_title="$(cut -d ',' -f2 <<< $(cut -d '>' -f3 <<< $1))"
+    class=$(hyprctl -j clients | jq -r " .[] | select(.address == \"$window_address\") | \"\(.initialClass)\"")
     
     case $class in 
-      zen-twilight) "${IPC_SCRIPTDIR}/zen_opacity_switch.sh $window_address $window_title";; 
+      zen-twilight) 
+      echo "hello 2"
+      "${IPC_SCRIPTDIR}/zen_opacity_switch.sh" $window_address $window_title;; 
     esac
 }
 
 handle() {
   case $1 in
-    windowtitlev2) identify_window_initial_class $1;;
+    windowtitlev2*) 
+    identify_window_initial_class $1
+    ;;
   esac
 }
 
--- modules/programs/hyprland/scripts/IPC/zen_opacity_switch.sh
+++ modules/programs/hyprland/scripts/IPC/zen_opacity_switch.sh
@@ -2,11 +2,10 @@
 window_address=$1
 window_title=$2
 
-hyprctl keyword "windowrule[opaque-zen-browser]:match:address $window_adress"
-hyprctl keyword "windowrule[transparent-zen-browser]:match:address $window_adress"
+echo "hey" > ~/test.txt
 
 case "$window_title" in
-    Crunchyroll || Watch on Crunchyroll)
+    *Crunchyroll*)
         hyprctl keyword 'windowrule[opaque-zen-browser]:enable true'
         hyprctl keyword 'windowrule[transparent-zen-browser]:enable false'
         ;;
