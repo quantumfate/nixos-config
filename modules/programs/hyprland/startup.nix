f7bf47b96d93574bc0353b1a848f85ee16b85729
Leon Connor Holm <leonconnorholm@gmail.com>
2025-12-12 16:03:11 +0100

    Implemented an opacity switch for Zen Browser

--- modules/programs/hyprland/startup.nix
+++ modules/programs/hyprland/startup.nix
@@ -3,6 +3,7 @@
 let
   userCfg = config.common.user;
   primaryMonitor = config.common.peripherals.primaryMonitor;
+  scriptDir = "/home/${userCfg.name}/.config/hypr/scripts/IPC";
 in {
   home-manager.users."${userCfg.name}" = {
     wayland.windowManager.hyprland = {
@@ -10,6 +11,7 @@ in {
         "exec-once" = [
           "xrandr --output ${primaryMonitor.name} --primary --mode ${primaryMonitor.mode} --rate ${primaryMonitor.rate}"
           "waybar"
+          "${scriptDir}/IPC_Wrapper.sh"
         ];
       };
     };
