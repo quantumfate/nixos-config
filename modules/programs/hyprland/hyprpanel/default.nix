e44f6305a1e3a424c56db017077444574edf686c
Leon Connor Holm <leonconnorholm@gmail.com>
2025-12-23 22:15:35 +0100

    Added Hyprpanel

--- modules/programs/hyprland/hyprpanel/default.nix
+++ modules/programs/hyprland/hyprpanel/default.nix
@@ -3,7 +3,7 @@
 let userCfg = config.common.user;
 in {
   home-manager.users."${userCfg.name}" = {
-    home.packages = with pkgs; [ hyprpanel ];
+    home.packages = with pkgs; [ hyprpanel libnotify ];
     home.file.".config/hyprpanel/config.json" = { source = ./config.json; };
   };
 
