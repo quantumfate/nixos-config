7bc333a24b5259a18bcd8815a5e8d5a3c890f03e
Leon Connor Holm <62796852+quantumfate@users.noreply.github.com>
2026-01-06 17:10:50 +0100

    Added fix for vesktop

--- modules/programs/vesktop.nix
+++ modules/programs/vesktop.nix
@@ -1,10 +1,32 @@
 { config, ... }:
 
-let 
+let
   userCfg = config.common.user;
   catppuccin = config.theme.catppuccin;
 in {
+  # https://github.com/NixOS/nixpkgs/issues/476669#issuecomment-3707850520
+  nixpkgs.overlays = [
+    (final: prev: {
+      vesktop = prev.vesktop.overrideAttrs (old: {
+        preBuild = ''
+          cp -r ${prev.electron.dist} electron-dist
+          chmod -R u+w electron-dist
+        '';
+        buildPhase = ''
+          runHook preBuild
 
+          pnpm build
+          pnpm exec electron-builder \
+            --dir \
+            -c.asarUnpack="**/*.node" \
+            -c.electronDist="electron-dist" \
+            -c.electronVersion=${prev.electron.version}
+
+          runHook postBuild
+        '';
+      });
+    })
+  ];
   home-manager.users."${userCfg.name}" = {
     programs.vesktop = {
       enable = true;
