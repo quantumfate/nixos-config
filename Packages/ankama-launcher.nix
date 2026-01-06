0fe70a46bfd81f93cb59966aa85ad8d13e210766
Leon Connor Holm <62796852+quantumfate@users.noreply.github.com>
2026-01-06 17:29:36 +0100

    Updated submodules

--- Packages/ankama-launcher.nix
+++ Packages/ankama-launcher.nix
@@ -14,7 +14,8 @@ let
   src = fetchurl {
     url = "https://download.ankama.com/launcher-dofus/full/linux";
     name = "ankama-launcher-bypass.AppImage";
-    hash = "sha256-dWpBY/8clQT16lIPUR+y346MwRsHPU0M4ir/E9BqpwE=";
+    # dWpBY/8clQT16lIPUR+y346MwRsHPU0M4ir/E9BqpwE=
+    hash = "";
     curlOptsList = [
       "-A" "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
       "-H" "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"
