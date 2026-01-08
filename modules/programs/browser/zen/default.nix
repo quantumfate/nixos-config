29691ba6e310fbf7e9b309b1a8439ea26b2d37d9
Leon Connor Holm <62796852+quantumfate@users.noreply.github.com>
2026-01-08 14:01:02 +0100

    Migrated zen browser to personal flake

--- modules/programs/browser/zen/default.nix
+++ modules/programs/browser/zen/default.nix
@@ -17,28 +17,24 @@ let
 
   flavor = capitalize config.common.style.catppuccin.flavor;
   accent = capitalize config.common.style.catppuccin.accent;
-in {
-  imports = [
-    "${inputs.my-zen-config}/bookmarks.nix"
-    "${inputs.my-zen-config}/profiles.nix"
+
+  zen_profiles = [
+    ".zen/dofus/zen-keyboard-shortcuts.json"
+    ".zen/default/zen-keyboard-shortcuts.json"
   ];
+in {
   home-manager.users."${userCfg.name}" = {
     imports = [ inputs.zen-browser.homeModules.twilight ];
     home.file = lib.attrsets.genAttrs catppuccinTargets (path: {
       source = "${catppuccinZenTheme}/themes/${flavor}/${accent}/";
       recursive = true;
-    });
+    }) // lib.attrsets.genAttrs zen_profiles
+      (profile: { source = ./kb-shortcuts.json; });
 
     programs.zen-browser = {
       enable = true;
       nativeMessagingHosts = [ pkgs.firefoxpwa ];
-      policies = let
-        mkExtensionSettings = builtins.mapAttrs (_: pluginId: {
-          install_url =
-            "https://addons.mozilla.org/firefox/downloads/latest/${pluginId}/latest.xpi";
-          installation_mode = "force_installed";
-        });
-      in {
+      policies = {
         AutofillAddressEnabled = true;
         AutofillCreditCardEnabled = false;
         DisableAppUpdate = true;
@@ -60,18 +56,8 @@ in {
           Cryptomining = true;
           Fingerprinting = true;
         };
-        ExtensionSettings = mkExtensionSettings {
-          "{32a6dd4b-a3e6-4571-bc4c-9127d1787096}" = "KeePassXC-Browser";
-          "{85860b32-02a8-431a-b2b1-40fbd64c9c69}" = "github-file-icons";
-          "dofus-translate@isthos.com" = "Dofus Translator";
-          "{e74f2cbf-fccb-4eee-bca3-7b490bc986cb}" =
-            "Unhook - Remove YouTube Recommended & Shorts";
-          "{bbb880ce-43c9-47ae-b746-c3e0096c5b76}" =
-            "Catppuccin for Web File Explorer Icons";
-          "{295bb111-99ed-415c-8f6a-a731ec1d8123}" = "Dark Reader";
-          "{f0b41b17-1558-42c8-b535-f162c07f2642}" = "uBlock Origin";
-        };
       };
+      profiles = inputs.my-zen-config;
     };
   };
 }
