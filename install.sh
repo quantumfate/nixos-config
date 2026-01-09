3e8bba5fc7ae8b41d2d8adeebc786b5720263c46
Leon Connor Holm <62796852+quantumfate@users.noreply.github.com>
2026-01-09 10:52:40 +0100

    Nix Flake Update

--- install.sh
+++ install.sh
@@ -7,17 +7,17 @@ cd "$HOME/Projects/github/nixos-config" || exit 1
 HOST=$(hostname)
 
 if [[ "$1" == "--upgrade" || "$1" == "-u" ]]; then
-    pkill zen-twilight
+    pkill zen-beta
     sudo nixos-rebuild switch --upgrade $ADDITIONAL_FLAGS --flake "$HOME/Projects/github/nixos-config#$HOST"
 elif [[ "$1" == "--rebuild" || "$1" == "-r" ]]; then
-    pkill zen-twilight
+    pkill zen-beta
     sudo nixos-rebuild switch $ADDITIONAL_FLAGS --flake "$HOME/Projects/github/nixos-config#$HOST"
 elif [[ "$1" == "--flake-update" || "$1" == "-f" ]]; then
     nix flake update
     git add $HOME/Projects/github/nixos-config
     git commit -m "Nix Flake Update"
     git push
-    pkill zen-twilight
+    pkill zen-beta
     sudo nixos-rebuild switch --upgrade $ADDITIONAL_FLAGS --flake "$HOME/Projects/github/nixos-config#$HOST"
 else
 	echo "usage: ./install.sh [-u | --upgrade] [-r | --rebuild] [-f | --flake-update ] [<args passed to nixos-rebuild switch>]"
