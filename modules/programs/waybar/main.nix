9d03a68da24940c4bd733fec29d93dc5c9b993c4
Leon Connor Holm <62796852+quantumfate@users.noreply.github.com>
2026-01-14 16:51:10 +0100

    Nix Flake Update

--- modules/programs/waybar/main.nix
+++ modules/programs/waybar/main.nix
@@ -37,31 +37,35 @@ let
       "custom/power"
     ];
   };
-in {
-  config = lib.mkIf config.common.modules.waybar.enable {
-    home-manager.users."${userCfg.name}" = {
-      programs.waybar = {
-        enable = true;
-        settings = {
-          mainBar = {
-            include = "/home/${userCfg.name}/.config/waybar/modules";
-            position = "top";
-            height = 25;
-            ipc = true;
-            margin-top = 10;
-            margin-right = 0;
-            margin-left = 0;
-            layer = "top";
-            spacing = 0;
-            fixed-center = true;
-            output = [
-              "${config.common.peripherals.primaryMonitor.name}"
-            ]; # TODO: set a list of active monitors
-            modules-left =
-              [ "hyprland/workspaces" "cpu" "memory" "disk" "network" ];
-            modules-center = [ "hyprland/window" ];
-            modules-right = modulesRight.${hostName} or modulesRight.default;
-          };
+in
+{
+  home-manager.users."${userCfg.name}" = {
+    programs.waybar = {
+      enable = true;
+      settings = {
+        mainBar = {
+          include = "/home/${userCfg.name}/.config/waybar/modules";
+          position = "top";
+          height = 25;
+          ipc = true;
+          margin-top = 10;
+          margin-right = 0;
+          margin-left = 0;
+          layer = "top";
+          spacing = 0;
+          fixed-center = true;
+          output = [
+            "${config.common.peripherals.primaryMonitor.name}"
+          ]; # TODO: set a list of active monitors
+          modules-left = [
+            "hyprland/workspaces"
+            "cpu"
+            "memory"
+            "disk"
+            "network"
+          ];
+          modules-center = [ "hyprland/window" ];
+          modules-right = modulesRight.${hostName} or modulesRight.default;
         };
       };
     };
